e47f8284ecc6396a5940c8bc33edeccf24171705
VikaNoumenko <vika.noumenko@gmail.com>
2017-10-20 00:00:46 +0400

    how to return a value of an object

--- src/main/java/js/main.js
+++ src/main/java/js/main.js
@@ -1,16 +1,6 @@
-function  saySomething(phrase) {
-    console.log(" you said: " + phrase);
-
+var cat = {
+    "name " : " cat",
+    "food" : "tuna"
 }
 
-function getPhraseLength(phrase, another) {
-    var l = phrase.length + another.length;
-    return l;
-}
-
-var p1 = "long";
-var p2 = "short";
-
-var thisLength = getPhraseLength(p1, p2);
-
-console.log(thisLength);
+cat.food;
