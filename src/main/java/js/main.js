81468d27f74a6343b35ab3f444022a31acb219f5
VikaNoumenko <vika.noumenko@gmail.com>
2017-10-19 23:17:53 +0400

    how to grab a value from input

--- src/main/java/js/main.js
+++ src/main/java/js/main.js
@@ -1,22 +1,9 @@
 var button = document.getElementById("hello-button");
 
-function buttonClick(){
+function buttonClick() {
     console.log("Button clicked");
-   // button.removeEventListener("click", buttonClick);
-    //return an object with variable:
-    document.getElementById("text").innerHTML = " Don't click!";
+
+    var customText = document.getElementsByClassName("input");
+    var results = document.getElementById("text");
+    results.innerHTML = " Hello, " + customText[0].value;
 }
-
-//button.addEventListener("click", buttonClick);
-var activities = ["go", "see"];
-//removes last item in array:
-console.log(activities.pop());
-console.log(activities.push("walk"));
-//like pop method:
-activities.shift();
-//at the beginning:
-activities.unshift("eat");
-
-activities.forEach(function (item, index) {
-    console.log(" items", item, index);
-});
