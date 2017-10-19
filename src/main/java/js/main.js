a9370ef1490844d08b812acaf6a60749535dc5b4
VikaNoumenko <vika.noumenko@gmail.com>
2017-10-19 21:53:18 +0400

    innerHTML

--- src/main/java/js/main.js
+++ src/main/java/js/main.js
@@ -2,7 +2,9 @@ var button = document.getElementById("hello-button");
 
 function buttonClick(){
     console.log("Button clicked");
-    button.removeEventListener("click", buttonClick);
+   // button.removeEventListener("click", buttonClick);
+    //return an object with variable:
+    document.getElementById("text").innerHTML = " Don't click!";
 }
 
-button.addEventListener("click", buttonClick);
+//button.addEventListener("click", buttonClick);
