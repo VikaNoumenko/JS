e74227d61f5cd266a6791d0a9ca2529d1fa00e94
VikaNoumenko <vika.noumenko@gmail.com>
2017-10-19 21:45:36 +0400

    event listeners

--- src/main/java/js/main.js
+++ src/main/java/js/main.js
@@ -1,3 +1,8 @@
+var button = document.getElementById("hello-button");
+
 function buttonClick(){
     console.log("Button clicked");
+    button.removeEventListener("click", buttonClick);
 }
+
+button.addEventListener("click", buttonClick);
